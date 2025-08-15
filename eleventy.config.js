f9b4a440d4939e845647ac2bfbdf071d884b285a
Phil Hawksworth <phil@deno.com>
2025-08-15 13:02:19 +0100

    initial commit 🚀

---
+++ eleventy.config.js
@@ -0,0 +1,12 @@
+import { VentoPlugin } from 'eleventy-plugin-vento';
+export default async function(eleventyConfig) {
+  eleventyConfig.addPlugin(VentoPlugin);
+  eleventyConfig.addPassthroughCopy("src/wallpapers");
+};
+
+export const config = {
+  dir: {
+    input: "src",
+    output: "dist"
+  }
+};
---
+++ pre-build.ts
@@ -0,0 +1,11 @@
+// generate a data object from all the crocs images
+const imagesDir = `src/wallpapers`;
+const files = [];
+for await (const file of Deno.readDir(imagesDir)) {
+  files.push({
+    // make the name titlecase and replace - with space
+    "name": file.name
+  });
+}
+
+await Deno.writeTextFile("src/_data/wallpapers.json", JSON.stringify(files, null, 2));
